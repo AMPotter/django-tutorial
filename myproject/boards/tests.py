f16377eca2141a0f74f863ce29ff9e2a68ceb890
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2018-10-13 13:50:44 -0700

    test now passes - correctly?

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -3,6 +3,6 @@ from django.test import TestCase
 
 class HomeTests(TestCase):
     def test_home_view_status_code(self):
-        url = 'home'
+        url = ''
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
