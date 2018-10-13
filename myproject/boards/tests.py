848aeb7bad0094cdace5f4d0fdffc598829b63d3
Andrew Michael Potter <i.am.andrew.potter@gmail.com>
2018-10-13 13:49:19 -0700

    first test, modified from tutorial

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -1,3 +1,8 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class HomeTests(TestCase):
+    def test_home_view_status_code(self):
+        url = 'home'
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
